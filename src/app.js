3364b3a7ad84dc8687cc5111de9820aec5c33150
Sabrina Estévez <144539845+SabrinaESMO@users.noreply.github.com>
2023-12-16 00:57:27 +0000

    Instrucciones básicas completadas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+let logo = ["♦", "♥", "♠", "♣"];
+let number = ["A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+let randomLogo = Math.floor(Math.random() * logo.length);
+let randomNumber = Math.floor(Math.random() * number.length);
+
+let showLogo = logo[randomLogo];
+let showNumber = number[randomNumber];
+
+document.querySelector(".icon-top").innerHTML = showLogo;
+document.querySelector(".number").innerHTML = showNumber;
+document.querySelector(".icon-bottom").innerHTML = showLogo;
+
+let element1 = document.querySelector(".icon-top");
+if (showLogo == "♦" || showLogo == "♥") {
+  element1.classList.add("red");
+} else {
+  element1.classList.add("black");
+}
+
+let element2 = document.querySelector(".icon-bottom");
+if (showLogo == "♦" || showLogo == "♥") {
+  element2.classList.add("red-down");
+} else {
+  element2.classList.add("black-down");
+}
